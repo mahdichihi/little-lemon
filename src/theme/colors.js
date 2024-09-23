12c5876d67cda62383b1b79d199d49542e68325b
mahdichihi <mahdichihi95@gmail.com>
2024-09-23 12:09:04 +0100

    adding colors theme

---
+++ src/theme/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primary1: "#495E57",
+  primary2: "#F4CE14",
+
+  secondary1: "#EE9972",
+  secondary2: "#FBDABB",
+
+  highlight1: "#EDEFEE",
+  highlight2: "#333333",
+};
