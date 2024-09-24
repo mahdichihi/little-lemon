a3c051094dd6fc854e871f51405f86e0a41ddac5
mahdichihi <mahdichihi95@gmail.com>
2024-09-24 17:32:50 +0100

    creating about section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import styles from "./About.module.css";
+import DisplayTitle from "../../components/typography/DisplayTitle";
+import SubTitle from "../../components/typography/SubTitle";
+import LeadText from "../../components/typography/LeadText";
+import IMG1 from "../../assets/images/marioAndAdrianA.jpg";
+import IMG2 from "../../assets/images/marioAndAdrianB.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.contentContainer}>
+          <DisplayTitle>Little Lemon</DisplayTitle>
+          <SubTitle>Chicago</SubTitle>
+          <img src={IMG1} alt="Mario and Adrian" className={styles.img} />
+
+          <div className={styles.leadText}>
+            <LeadText>
+              Mario and Adrian, the dreamers behind Little Lemon, are not just
+              founders; they’re the heart of the kitchen. With passion as their
+              main ingredient, they’re on a mission to turn their culinary
+              dreams into a dining experience you'll never forget.
+            </LeadText>
+          </div>
+        </div>
+        <div className={styles.imgsContainer}>
+          <img src={IMG1} loading="lazy" alt="Mario and Adrian" />
+          <img src={IMG2} loading="lazy" alt="Mario and Adrian" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
